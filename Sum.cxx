120171d28e9023ec4950ac2f6bfff65a04066029
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2020-10-20 11:22:39 +0530

    Finding_Sum

---
+++ Sum.cxx
@@ -0,0 +1,13 @@
+#import<iostream>
+using namespace std;
+void main(){
+	cout<<"Enter A Number\n";
+	int a;
+	cin>>a;
+	int b;
+	cout<<"Enter Second Number\n";
+	 cin>>b;
+	int c= a + b;
+	cout<<"The Sum Is "<<c;
+	
+}
